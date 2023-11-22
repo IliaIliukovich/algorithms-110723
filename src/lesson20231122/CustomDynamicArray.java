e2ef500da9b1d51f8954ad9f849d759369880435
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2023-11-22 14:08:59 +0100

    Dynamic arrays

--- src/lesson20231122/CustomDynamicArray.java
+++ src/lesson20231122/CustomDynamicArray.java
@@ -1,8 +1,9 @@
 package lesson20231122;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
-public class CustomDynamicArray {
+public class CustomDynamicArray implements Iterable<Integer> {
 
     private int[] data;
     private int count;
@@ -20,15 +21,15 @@ public class CustomDynamicArray {
         this.size = size;
     }
 
-    public void add(int element){
+    public void add(int element){ // O(1) amortized,  O(n) - worst case
         if (count == size){
             growSize();
         }
         data[count++] = element;
     }
 
-    public void addAt(int index, int element){
-        if (index >= count) throw new RuntimeException("Index is out of bounds");
+    public void addAt(int index, int element){ // O(n)
+        if (index < 0 || index >= count) throw new RuntimeException("Index is out of bounds");
         if (count == size){
             growSize();
         }
@@ -48,7 +49,7 @@ public class CustomDynamicArray {
         size = size * 2;
     }
 
-    public void shrinkSize(){
+    public void shrinkSize(){ // O(n)
         if (count < size){
             int[] newArray = new int[count];
             for (int i = 0; i < count; i++) {
@@ -59,36 +60,45 @@ public class CustomDynamicArray {
         }
     }
 
-    public void remove(){
-        data[--count] = 0; // make link to objects null for GC
+    public void remove(){ // O(1)
+        data[--count] = 0; // make link to object null for GC
     }
 
-    public void removeAt(int index){
+    public void removeAt(int index){ // O(n)
+        if (index < 0 || index >= count) throw new RuntimeException("Index is out of bounds");
 
+        for (int i = index; i < count; i++) {
+            data[i] = data[i+1];
+        }
+        data[--count] = 0;
     }
 
-    public void set(int index, int element){
-
+    public void set(int index, int element){ // O(1)
+        if (index < 0 || index >= count) throw new RuntimeException("Index is out of bounds");
+        data[index] = element;
     }
 
-    public int get(int index){
-        return 0;
+    public int get(int index){ // O(1)
+        if (index < 0 || index >= count) throw new RuntimeException("Index is out of bounds");
+        return data[index];
     }
 
-    public void clear(){
-
+    public void clear(){ // O(1)
+        data = new int[0];
+        size = 1;
+        count = 0;
     }
 
-    public int contains(int element){
-        return 0;
+    public int contains(int element){ // O(n)
+        for (int i = 0; i < count; i++) {
+            if (data[i] == element) return i;
+        }
+        return -1;
     }
 
-    public boolean isEmpty(){
-        return false;
+    public boolean isEmpty(){ // O(1)
+        return count == 0;
     }
-
-
-
 
     public static void main(String[] args) {
         CustomDynamicArray array = new CustomDynamicArray();
@@ -114,6 +124,21 @@ public class CustomDynamicArray {
         array.remove();
         System.out.println(array);
         array.printInnerStructure();
+
+        array.removeAt(2);
+        array.printInnerStructure();
+
+        System.out.println(array.contains(20));
+        System.out.println(array.contains(0));
+
+        array.add(0);
+        System.out.println(array.contains(0));
+        System.out.println(array);
+
+        System.out.println("Loop with iterator:");
+        for (Integer e : array) {
+            System.out.println(e);
+        }
     }
 
     public void printInnerStructure(){
@@ -122,11 +147,26 @@ public class CustomDynamicArray {
 
     @Override
     public String toString() {
-        String result = "CustomDynamicArray[";
+        StringBuilder result = new StringBuilder("CustomDynamicArray[");
         for (int i = 0; i < count; i++) {
-            result += data[i] + ", ";
+            result.append(data[i]).append(", ");
         }
-        result += "]";
-        return result;
+        result.append("]");
+        return result.toString();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() { // TODO
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public Integer next() {
+                return null;
+            }
+        };
     }
 }
